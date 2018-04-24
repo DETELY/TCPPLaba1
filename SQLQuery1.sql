82204b2313c331306d30277c587ce3a10ffeda63
DETELY <DETELY@users.noreply.github.com>
2018-04-24 23:07:07 +0300

    add SQLQuery1

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿GO    
+ALTER TABLE Client
+ADD CONSTRAINT FK_Client_Holdings FOREIGN KEY (clientID)     
+    REFERENCES Holdings (clientID)  
+    ON DELETE CASCADE    
+    ON UPDATE CASCADE    
+;    
+GO  
